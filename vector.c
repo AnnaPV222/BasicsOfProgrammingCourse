244422a23e983e6c6279cefc8c489991620963aa
AnnaPV222 <sergei30062003@gmail.com>
2023-06-11 14:40:09 +0300

    first commit

---
+++ vector.c
@@ -0,0 +1,5 @@
+//
+// Created by Мой ПК on 11.06.2023.
+//
+
+#include "vector.h"
---
+++ vector.h
@@ -0,0 +1,8 @@
+//
+// Created by Мой ПК on 11.06.2023.
+//
+
+#ifndef VEC_VECTOR_H
+#define VEC_VECTOR_H
+
+#endif //VEC_VECTOR_H
