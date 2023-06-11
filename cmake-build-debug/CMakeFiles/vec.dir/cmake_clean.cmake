e4b911b00cba40b6b9066a7f20d62533e4742ace
AnnaPV222 <sergei30062003@gmail.com>
2023-06-11 14:34:40 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/vec.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vec.dir/main.c.obj"
+  "libvec.dll.a"
+  "vec.exe"
+  "vec.exe.manifest"
+  "vec.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/vec.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
